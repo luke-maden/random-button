cae365a7c239e55dcfac551004c6a5954d7b22d5
luke-maden <madenluke@gmail.com>
2025-06-04 21:03:21 -0700

    working prototype, prints notification with random album title.

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,18 @@
 import fetchAllAlbums from "./fetchAllAlbums";
-import getRandomAlbum from "./getRandomAlbum"
 
 async function main() {
   while (!Spicetify?.showNotification || !Spicetify?.Platform.LibraryAPI) {
     await new Promise(resolve => setTimeout(resolve, 100));
   }
 
-  let albums = fetchAllAlbums();
+  let albums = await fetchAllAlbums();
 
-  console.log((await albums).items[0].name);
+  console.log(albums.items[0].name);
   
   // Show message on keybind.
-  Spicetify.Mousetrap.bind('R', async function(randAlbum = getRandomAlbum()) {
-    Spicetify.showNotification((await albums).items[0].name);
+  Spicetify.Mousetrap.bind('R', function() {
+    let index = Math.floor(Math.random() * albums.totalLength);
+    Spicetify.showNotification(albums.items[index].name);
   });
 }
 
